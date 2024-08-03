011ac2ef53b9cd6b5143b51f3d9450e7ed5d81ad
webflyx.ceo:ThePrimeagen <leroitz@gmail.com>
2024-08-03 07:39:21 +0200

    at count characters

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        return f.read()
+
+def number_of_words():
+    text = main().split
+    words = len(text())
+    return words
+
+def count_characters():
+    text= main().lower()
+    dictionary_of_book= {}
+    for i in text:
+        dictionary_of_book[i] = dictionary_of_book.get(i,0) +1
+    print(dictionary_of_book)
+
+
+
+count_characters()
+
+
+
+
+
+    
