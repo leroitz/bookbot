6d9ccdbde3394eac5132288cfa59393d04003219
webflyx.ceo:ThePrimeagen <leroitz@gmail.com>
2024-08-03 11:12:52 +0200

    uhm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def main():
     with open("books/frankenstein.txt") as f:
         return f.read()
@@ -7,16 +8,56 @@ def number_of_words():
     words = len(text())
     return words
 
+numbers = number_of_words()
+
 def count_characters():
     text= main().lower()
     dictionary_of_book= {}
     for i in text:
         dictionary_of_book[i] = dictionary_of_book.get(i,0) +1
-    print(dictionary_of_book)
+    return dictionary_of_book
+
+dictionary_of_book= {}
+text= main().lower()
+
+for i in text:
+    dictionary_of_book[i] = dictionary_of_book.get(i,0) +1
 
 
 
-count_characters()
+message= f"""---Begin report of books/frankenstein.txt---/n
+    {numbers} words found in a text
+    
+    dafadsf"""
+def sort_on(dictionary_of_book):
+    return dictionary_of_book[count_characters()]
+
+dictionary= [dictionary_of_book]
+
+dictionary.sort(reverse=True, key= sort_on)
+print(dictionary)
+
+
+
+
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
